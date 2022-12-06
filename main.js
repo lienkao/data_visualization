5841189b1ab4d32ecaa2cbe66436e02ec8fcb506
Sophie Lien <sophielien09@gmail.com>
2022-12-07 00:29:55 +0800

    bar chart's brush draw in drawBarChart()

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 // select the attribute show in bar chart
 const bar_chart_attribute_option = ["empty", "FB%", "KDA", "GPM", "KP%", "DMG%", "DPM", "VSPM"];
-
+let left_selected, center_selected, right_selected;
+// TODO
 let left_bar_fill_g, left_bar_x, left_bar_y, left_bar_histogram;
 let center_bar_fill_g, center_bar_x, center_bar_y, center_bar_histogram;
 let right_bar_fill_g, right_bar_x, right_bar_y, right_bar_histogram;
-let [left_attr, center_attr, right_attr] = ["KDA", "empty", "empty"];
+let [left_attr, center_attr, right_attr] = ["KDA", "GPM", "DPM"];
 
 const fixed_bins_nums = {"VSPM": 22, "DMG%": 25};
 const fixed_x_axis_domain = {"KDA": [0, 9], "GPM": [200, 500], "DPM": [100, 700], "FB%": [0, 1.1], "KP%": [0.3, 1.1], "DMG%": [0.05, 0.375], "VSPM": [0.5,3.5]}
@@ -89,46 +90,205 @@ d3.csv("data/players.csv", d3.autoType).then(players => {
       
       d3.select("#left-select")
         .on('change', d=>{
-          d3.select("#left-bar").selectAll('svg').remove();
-          left_attr = d3.select("#left-select").property("value");
-          [left_bar_fill_g, left_bar_x, left_bar_y, left_bar_histogram] = drawBarChart("#left-bar", players, players, left_attr, left_attr);
-        });                        
-      [left_bar_fill_g, left_bar_x, left_bar_y, left_bar_histogram] = drawBarChart("#left-bar", players, players, left_attr, left_attr);
-      [center_bar_fill_g, center_bar_x, center_bar_y, center_bar_histogram] = drawBarChart("#center-bar", players, players, center_attr, center_attr);
-      [right_bar_fill_g, right_bar_x, right_bar_y, right_bar_histogram] = drawBarChart("#right-bar", players, players, right_attr, right_attr);
+          left_attr = d3.select("#left-select").property("value");       
+      });  
+      d3.select("#center-select")
+        .on('change', d=>{
+          center_attr = d3.select("#center-select").property("value");       
+      });    
+      [left_bar_fill_g, left_bar_x, left_bar_y, left_bar_histogram] = drawBarChart("#left-bar", players, players, left_attr, left_attr, 'left');
+      [center_bar_fill_g, center_bar_x, center_bar_y, center_bar_histogram] = drawBarChart("#center-bar", players, players, center_attr, center_attr, 'center');
+      [right_bar_fill_g, right_bar_x, right_bar_y, right_bar_histogram] = drawBarChart("#right-bar", players, players, right_attr, right_attr, 'right');
 
-      // #region
-      // console.log(pts_fill_g)
-      // ----- Left Bar Chart -----
-      let brush_left = d3.brushX()
-                      .extent([[0, 0], [WIDTH_bar, HEIGHT_bar]])
-                      .on('start brush', brushed_left)
-                      .on('end', endBrushed_left)
-      let left_selected = left_bar_x.domain();
-      left_bar_fill_g.append('g')
-                .attr('class', 'brush-bar')
-                .call(brush_left)
-      // ----- Center Bar Chart -----
-      let brush_center = d3.brushX()
-                      .extent([[0, 0], [WIDTH_bar, HEIGHT_bar]])
-                      .on('start brush', brushed_center)
-                      .on('end', endBrushed_center)
-      let center_selected = center_bar_x.domain();
-      center_bar_fill_g.append('g')
-                .attr('class', 'brush-bar')
-                .call(brush_center)
-      // ----- Right Bar Chart -----
-      let brush_right = d3.brushX()
-                      .extent([[0, 0], [WIDTH_bar, HEIGHT_bar]])
-                      .on('start brush', brushed_right)
-                      .on('end', endBrushed_right)
-      let right_selected = right_bar_x.domain();
-      right_bar_fill_g.append('g')
-                .attr('class', 'brush-bar')
-                .call(brush_right)
-      // #endregion
-      // ------------- functions for views -----------------
+      // // #region
+      // // console.log(pts_fill_g)  
+
+      // // ----- Left Bar Chart -----
+      // let brush_left = d3.brushX()
+      //                 .extent([[0, 0], [WIDTH_bar, HEIGHT_bar]])
+      //                 .on('start brush', brushed_left)
+      //                 .on('end', endBrushed_left)
+      // let left_selected = left_bar_x.domain();
+      // left_bar_fill_g.append('g')
+      //           .attr('class', 'brush-bar')
+      //           .call(brush_left)
+      // // ----- Center Bar Chart -----
+      // let brush_center = d3.brushX()
+      //                 .extent([[0, 0], [WIDTH_bar, HEIGHT_bar]])
+      //                 .on('start brush', brushed_center)
+      //                 .on('end', endBrushed_center)
+      // let center_selected = center_bar_x.domain();
+      // center_bar_fill_g.append('g')
+      //           .attr('class', 'brush-bar')
+      //           .call(brush_center)
+      // // ----- Right Bar Chart -----
+      // let brush_right = d3.brushX()
+      //                 .extent([[0, 0], [WIDTH_bar, HEIGHT_bar]])
+      //                 .on('start brush', brushed_right)
+      //                 .on('end', endBrushed_right)
+      // let right_selected = right_bar_x.domain();
+      // right_bar_fill_g.append('g')
+      //           .attr('class', 'brush-bar')
+      //           .call(brush_right)
+      // // #endregion
       
+
+      function reRenderingBarChart(selected_g, selected_players, x, y, histogram) {
+
+        fill_bins = histogram(selected_players);
+        // console.log(fill_bins);
+        selected_g.selectAll("rect")
+                .data(fill_bins)
+                .transition()
+                .duration(1000)
+                .attr("y", d => ( y(d.length))) 
+                // .attr("transform", d => `translate(${pts_x(d.x0)}, 0)`) 
+                .attr("height", d => HEIGHT_bar -y(d.length))
+        // console.log(selected_g)
+      } // reRenderingBarChart
+
+      function getCountryPlayersNum(countries, players) {
+        let country_player_num = {};
+        countries.forEach(element => {
+          country_player_num[element.Country] = 0;
+        });
+        // console.log(country_player_num);
+        players.forEach(element => {
+          country_player_num[element.Country] += 1;
+        });
+        return country_player_num;
+      } // getTeamPlayersNum
+      function drawBarChart(selected_div, all_players, selected_players, attribute, title, side) {
+        
+        
+        let svg = d3.select(selected_div).append("svg")
+                    .attr("width", WIDTH_bar + MARGIN_bar.LEFT + MARGIN_bar.RIGHT)
+                    .attr("height", HEIGHT_bar + MARGIN_bar.TOP + MARGIN_bar.BOTTOM)
+        let g = svg.append("g")
+                    .attr("transform", `translate(${MARGIN_bar.LEFT}, ${MARGIN_bar.TOP})`)
+        let tick_nums = 20;
+        if (attribute in fixed_bins_nums)
+          tick_nums = fixed_bins_nums[attribute];
+        
+        let x_domain = [d3.min(all_players, d => d[attribute]), d3.max(all_players, d => d[attribute])];
+
+        if (attribute in fixed_x_axis_domain) 
+          x_domain = fixed_x_axis_domain[attribute];
+
+        // X ticks
+        const x = d3.scaleLinear()
+            .domain(x_domain)
+            .range([0, WIDTH_bar])
+        
+
+        x_g = g.append("g")
+          .attr("transform", `translate(0, ${HEIGHT_bar})`)
+          .call(d3.axisBottom(x));
+
+        
+        let histogram = d3.histogram()
+                              .value(d => d[attribute])
+                              .domain(x.domain())
+                              .thresholds(x.ticks(tick_nums));
+            
+        const bins = histogram(all_players);
+        // console.log(bins);
+        // Y ticks
+        const y = d3.scaleLinear()
+          .domain([0, d3.max(bins, d => d.length)])
+          .range([HEIGHT_bar, 0])
+          
+        g.append("g")
+          .call(d3.axisLeft(y))   
+        g.selectAll("rect")
+          .data(bins)
+          .enter().append("rect")
+          .attr("x", d => x(d.x0))
+          .attr("y", d => (y(d.length)))
+          .attr("width", d => x(d.x1)-x(d.x0))
+          .attr("height", d => HEIGHT_bar-y(d.length))
+            .style("fill",  `rgba(255, 255, 255, 100)`)
+            .attr('stroke', `rgba(0, 0, 0, 50)`)
+            .attr('stroke-width', "0.5px")
+        svg.append("text")
+          .attr("transform", `translate(${MARGIN_bar.LEFT+ 10}, ${MARGIN_bar.TOP})`)
+          .attr("font-size", fontSize_bar)
+          .text(title) 
+        let fill_g = svg.append("g")
+          .attr("transform", `translate(${MARGIN_bar.LEFT}, ${MARGIN_bar.TOP})`)
+        const fill_bins = histogram(selected_players);
+        // console.log(fill_bins)
+        // console.log(fill_bins);
+        fill_g.selectAll("rect")
+          .data(fill_bins)
+          .enter()
+          .append("rect")
+            // .attr('class', `${title}-bin`)
+            .attr("x", d => x(d.x0))
+            .attr("y", d => y(d.length))
+            .attr("width", d => x(d.x1)-x(d.x0))
+            .attr("height", d => HEIGHT_bar- y(d.length))
+            .style("fill", "rgba(105, 179, 162, 16)")
+            .attr('stroke', 'black')
+            .attr('stroke-width', "0.5px")
+          // console.log(fill_g)
+        let brush = d3.brushX()
+            .extent([[0, 0], [WIDTH_bar, HEIGHT_bar]])
+            .on('start brush', d=>{brushed(side, x)})
+            .on('end', d=>{endBrushed(side, x)})
+        if (side == 'left')
+          left_selected = x.domain();
+        else if (side == 'center')
+          center_selected = x.domain();
+        else
+          right_selected = x.domain();
+        fill_g.append('g')
+          .attr('class', 'brush-bar')
+          .call(brush)
+        return [fill_g, x, y, histogram];
+      } // drawBarChart
+
+      //#region brush for bar charts
+      function brushed(side, x) {
+        if (d3.event.selection==null) return;
+        var extent = d3.event.selection;//console.log(extent, d3.event.selection);
+        let reverse_x = d3.scaleLinear()
+                      .domain(x.range())
+                      .range(x.domain())
+        // console.log(reverse_x(extent[0]), reverse_x(extent[1]));
+        if (side=='left')
+          left_selected = [reverse_x(extent[0]), reverse_x(extent[1])];
+        else if (side == 'center')
+          center_selected = [reverse_x(extent[0]), reverse_x(extent[1])];
+        else
+          right_selected = [reverse_x(extent[0]), reverse_x(extent[1])];
+          console.log('left:', left_selected, 'center:', center_selected, 'right: ', right_selected);
+      } // brushed
+      function endBrushed(side, x) {
+        if (isBrushedScatter) {
+          isBrushedScatter = false;
+          d3.select('.brush')
+            .call(brush.clear)
+        }
+        if (d3.event.selection==null) {
+          if (side == 'left')
+            left_selected = x.domain();
+          else if (side == 'center')
+            center_selected = x.domain();
+          else
+            right_selected = x.domain();
+          console.log('left:', left_selected, 'center:', center_selected, 'right: ', right_selected);
+        }
+        else {
+          isBrushedBar = true;
+        }
+        let selected_players = filter_bar_selected();
+        transitionSelection(selected_players, true);
+      } // endBrushed
+
+      //#endregion
+
+            
       function transitionSelection (selected_players, isBar) {
         let country_selected_player_num = getCountryPlayersNum(countries, selected_players);
         country_dots.selectAll("circle")
@@ -155,188 +315,14 @@ d3.csv("data/players.csv", d3.autoType).then(players => {
           if ((player[left_attr] >= left_selected[0] && player[left_attr] <= left_selected[1]) &&
               (player[center_attr] >= center_selected[0] && player[center_attr] <= center_selected[1]) &&
               (player[right_attr] >= right_selected[0] && player[right_attr] <= right_selected[1]) 
-             )
+            )
             selected_players.push(player);
         })
         
         return selected_players;
       }
-      // ------------- brush for bar charts -------------------
-      //#region 
-      function brushed_left() {
-        if (d3.event.selection==null) return;
-        var extent = d3.event.selection;//console.log(extent, d3.event.selection);
-        let reverse_x = d3.scaleLinear()
-                      .domain(left_bar_x.range())
-                      .range(left_bar_x.domain())
-        // console.log(reverse_x(extent[0]), reverse_x(extent[1]));
-        
-        left_selected = [reverse_x(extent[0]), reverse_x(extent[1])];
-      } // brushed_left
-      function endBrushed_left() {
-        if (isBrushedScatter) {
-          isBrushedScatter = false;
-          d3.select('.brush')
-            .call(brush.clear)
-        }
-        if (d3.event.selection==null) {
-          left_selected = left_bar_x.domain();
-        }
-        else {
-          isBrushedBar = true;
-        }
-        let selected_players = filter_bar_selected();
-        transitionSelection(selected_players, true);
-      } // endBrushed_left
-      function brushed_center() {
-        if (d3.event.selection==null) return;
-        var extent = d3.event.selection;
-        let reverse_x = d3.scaleLinear()
-                      .domain(center_bar_x.range())
-                      .range(center_bar_x.domain())
-        
-        center_selected = [reverse_x(extent[0]), reverse_x(extent[1])];
-      } // brushed_center
-      function endBrushed_center() {
-        if (isBrushedScatter) {
-          isBrushedScatter = false;
-          d3.select('.brush')
-            .call(brush.clear)
-        }
-        if (d3.event.selection==null) {
-          center_selected = center_bar_x.domain();
-        } else {
-          isBrushedBar = true;
-        }
-        let selected_players = filter_bar_selected();
-        transitionSelection(selected_players, true);
-      } // endBrushed_center
-      function brushed_right() {
-        if (d3.event.selection==null) return;
-        var extent = d3.event.selection;
-        let reverse_x = d3.scaleLinear()
-                      .domain(right_bar_x.range())
-                      .range(right_bar_x.domain())
-        right_selected = [reverse_x(extent[0]), reverse_x(extent[1])];
-      } // brushed_right
-      function endBrushed_right() {
-        if (isBrushedScatter) {
-          isBrushedScatter = false;
-          d3.select('.brush')
-            .call(brush.clear)
-        }
-        if (d3.event.selection==null) right_selected = right_bar_x.domain();
-        else {
-          isBrushedBar = true;
-        }
-        let selected_players = filter_bar_selected();
-        transitionSelection(selected_players, true);
-      } // endBrushed_right
-      //#endregion
+
     }) // us-states.json
   }) // TeamLoc.csv
 }) // NBA1516.csv
 
-
-
-function reRenderingBarChart(selected_g, selected_players, x, y, histogram) {
-
-  fill_bins = histogram(selected_players);
-  // console.log(fill_bins);
-  selected_g.selectAll("rect")
-          .data(fill_bins)
-          .transition()
-          .duration(1000)
-          .attr("y", d => ( y(d.length))) 
-          // .attr("transform", d => `translate(${pts_x(d.x0)}, 0)`) 
-          .attr("height", d => HEIGHT_bar -y(d.length))
-  // console.log(selected_g)
-} // reRenderingBarChart
-
-function getCountryPlayersNum(countries, players) {
-  let country_player_num = {};
-  countries.forEach(element => {
-    country_player_num[element.Country] = 0;
-  });
-  // console.log(country_player_num);
-  players.forEach(element => {
-    country_player_num[element.Country] += 1;
-  });
-  return country_player_num;
-} // getTeamPlayersNum
-function drawBarChart(selected_div, all_players, selected_players, attribute, title) {
-  
-  
-  let svg = d3.select(selected_div).append("svg")
-              .attr("width", WIDTH_bar + MARGIN_bar.LEFT + MARGIN_bar.RIGHT)
-              .attr("height", HEIGHT_bar + MARGIN_bar.TOP + MARGIN_bar.BOTTOM)
-  let g = svg.append("g")
-              .attr("transform", `translate(${MARGIN_bar.LEFT}, ${MARGIN_bar.TOP})`)
-  let tick_nums = 20;
-  if (attribute in fixed_bins_nums)
-    tick_nums = fixed_bins_nums[attribute];
-  
-  let x_domain = [d3.min(all_players, d => d[attribute]), d3.max(all_players, d => d[attribute])];
-
-  if (attribute in fixed_x_axis_domain) 
-    x_domain = fixed_x_axis_domain[attribute];
-
-  // X ticks
-  const x = d3.scaleLinear()
-      .domain(x_domain)
-      .range([0, WIDTH_bar])
-  
-
-  g.append("g")
-    .attr("transform", `translate(0, ${HEIGHT_bar})`)
-    .call(d3.axisBottom(x));
-
-  
-  let histogram = d3.histogram()
-                        .value(d => d[attribute])
-                        .domain(x.domain())
-                        .thresholds(x.ticks(tick_nums));
-      
-  const bins = histogram(all_players);
-  // console.log(bins);
-  // Y ticks
-  const y = d3.scaleLinear()
-    .domain([0, d3.max(bins, d => d.length)])
-    .range([HEIGHT_bar, 0])
-    
-  g.append("g")
-    .call(d3.axisLeft(y))   
-  g.selectAll("rect")
-    .data(bins)
-    .enter().append("rect")
-    .attr("x", d => x(d.x0))
-    .attr("y", d => (y(d.length)))
-    .attr("width", d => x(d.x1)-x(d.x0))
-    .attr("height", d => HEIGHT_bar-y(d.length))
-      .style("fill",  `rgba(255, 255, 255, 100)`)
-      .attr('stroke', `rgba(0, 0, 0, 50)`)
-      .attr('stroke-width', "0.5px")
-  svg.append("text")
-    .attr("transform", `translate(${MARGIN_bar.LEFT+ 10}, ${MARGIN_bar.TOP})`)
-    .attr("font-size", fontSize_bar)
-    .text(title) 
-  let fill_g = svg.append("g")
-    .attr("transform", `translate(${MARGIN_bar.LEFT}, ${MARGIN_bar.TOP})`)
-  const fill_bins = histogram(selected_players);
-  // console.log(fill_bins)
-  // console.log(fill_bins);
-  fill_g.selectAll("rect")
-    .data(fill_bins)
-    .enter()
-    .append("rect")
-      // .attr('class', `${title}-bin`)
-      .attr("x", d => x(d.x0))
-      .attr("y", d => y(d.length))
-      .attr("width", d => x(d.x1)-x(d.x0))
-      .attr("height", d => HEIGHT_bar- y(d.length))
-      .style("fill", "rgba(105, 179, 162, 16)")
-      .attr('stroke', 'black')
-      .attr('stroke-width', "0.5px")
-    // console.log(fill_g)
-  return [fill_g, x, y, histogram];
-} // drawBarChart
